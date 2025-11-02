e40ca1bbe823dfa2ad09b62dd7862f7689d173ee
Aum <aumlette@gmail.com>
2025-11-02 21:51:23 +0000

    chore: update default apps script endpoint

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@
 // The user can always override this in the API settings within the app.
 // The API Key should NEVER be placed in this file.
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzIS0YD4D3yys_JY17vVy8LTzsadevCT-6sygcNzJj9rt2WKWyo9ZITUQP9ARvwFJqu/exec" // e.g., "https://script.google.com/macros/s/YOUR_ID/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxcOEauZPPmoTezBwgX-CdO1oJVLen5zVRE42vK44U5bPeramodEgi8SYadEWJVQle6/exec" // e.g., "https://script.google.com/macros/s/YOUR_ID/exec"
 };
